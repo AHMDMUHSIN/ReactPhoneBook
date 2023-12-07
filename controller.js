cadf00771c1ffb4edd845f8b35f762d04c61f306
shamil <shamilmohd418@gmail.com>
2023-12-07 21:28:21 +0530

    ug

--- controller.js
+++ controller.js
@@ -11,13 +11,13 @@ export async function getTask(req,res){
     res.status(200).send(task)
 }
 
-// export function delTask(req,res)
-// {
-//     const{id}=req.params;
-//     const data=schema.deleteOne({_id:id})
-//     data.then((resp)=>{
-//         res.status(200).send(resp)
-//     }).catch((error)=>{
-//         res.status(404).send(error)
-//     })
-// }
+export function delTask(req,res)
+{
+    const{id}=req.params;
+    const data=schema.deleteOne({_id:id})
+    data.then((resp)=>{
+        res.status(200).send(resp)
+    }).catch((error)=>{
+        res.status(404).send(error)
+    })
+}
---
+++ src/Edit/Edit.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Edit = () => {
+  return (
+    <div>
+      <div className="main-card">
+        <h2>Phone Book</h2>
+        <div><input type="text" placeholder='Name' name='name'/></div>
+        <div><input type="text" placeholder='Number' name='number'/></div>
+        <div><button>Register</button></div>
+
+       
+      </div>
+    </div>
+  )
+}
+
+export default Edit
